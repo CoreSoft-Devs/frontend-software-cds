86dac1e529266995517b3d1459ffd5ff9d960ecd
Luis Gabriel Janco <janco7249@gmail.com>
2023-10-11 17:59:51 -0400

    feat: add layout to root landing page

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Providers from "./Providers";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "CDS - Comunidad de Desarrollo de Software",
+  description:
+    "Aquí nos reunimos apasionados por la programación y el desarrollo de software para impulsar nuestro crecimiento y mejora constante. Nos enfocamos en todos los aspectos del desarrollo, desde web y móvil hasta backend y frontend, ¡y mucho más!",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
