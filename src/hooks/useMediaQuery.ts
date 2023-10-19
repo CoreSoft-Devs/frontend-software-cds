d40dd0072eca9907918d771d7efcf31ac5bce486
Luis Gabriel Janco <janco7249@gmail.com>
2023-10-19 12:19:14 -0400

    feat: add hook to get breakpoints  of device

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+type deviceState = "mobile" | "tablet" | "desktop" | null;
+type mediaQueryReturn = {
+  device: deviceState;
+  width: number | undefined;
+  height: number | undefined;
+  isMobile: boolean;
+  isTablet: boolean;
+  isDesktop: boolean;
+};
+type dimensionState = {
+  width: number;
+  height: number;
+} | null;
+
+export default function useMediaQuery(): mediaQueryReturn {
+  const [device, setDevice] = useState<deviceState>(null);
+  const [dimensions, setDimensions] = useState<dimensionState>(null);
+
+  useEffect(() => {
+    function checkDevice() {
+      if (window.matchMedia("(max-width: 640px)").matches) {
+        setDevice("mobile");
+      } else if (
+        window.matchMedia("(min-width: 641px) and (max-width: 1024px)").matches
+      ) {
+        setDevice("tablet");
+      } else {
+        setDevice("desktop");
+      }
+    }
+    // initial detection
+    checkDevice();
+    // listener for windows resize
+    window.addEventListener("resize", checkDevice);
+    // cleanup listener
+    return () => {
+      window.removeEventListener("resize", checkDevice);
+    };
+  }, []);
+
+  return {
+    device,
+    width: dimensions?.width,
+    height: dimensions?.height,
+    isMobile: device === "mobile",
+    isTablet: device === "tablet",
+    isDesktop: device === "desktop",
+  };
+}
