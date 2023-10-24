f25ac367baa8df7a4d90ca7648e40a18e297a69b
Luis Gabriel Janco <janco7249@gmail.com>
2023-10-24 00:08:43 -0400

    fix: set default width and height

--- src/components/shared/icons/sun.tsx
+++ src/components/shared/icons/sun.tsx
@@ -4,6 +4,8 @@ export default function Sun({ className }: { className?: string }) {
       stroke="currentColor"
       fill="none"
       strokeWidth={0}
+      width="20"
+      height="20"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       className={className}
