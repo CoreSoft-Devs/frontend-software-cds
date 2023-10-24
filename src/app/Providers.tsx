db7164170ff2031fef882011603fa48ebd81ebcf
Luis Gabriel Janco <janco7249@gmail.com>
2023-10-24 00:15:48 -0400

    style: add background gradient on providers

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -11,6 +11,7 @@ type Children = {
 const Providers = ({ children }: Children) => {
   return (
     <ThemeProvider>
+      <div className="z-0 fixed h-screen w-full bg-gradient-to-br dark:from-black from-white via-white dark:via-black to-cyan-100 dark:to-rose-950 opacity-50 dark:opacity-50" />
       <Suspense fallback="...">
         <Header />
       </Suspense>
