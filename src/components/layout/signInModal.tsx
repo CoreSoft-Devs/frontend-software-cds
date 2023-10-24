2deccbf20caf2a8d130fac3ad4127fe810032c28
Luis Gabriel Janco <janco7249@gmail.com>
2023-10-24 00:09:44 -0400

    refactor: remove unused variable

--- src/components/layout/signInModal.tsx
+++ src/components/layout/signInModal.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction, useState } from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import Modal from "../shared/modal";
 import Image from "next/image";
 import Link from "next/link";
