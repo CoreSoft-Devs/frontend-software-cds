18fce8cabc0389be869313ccef07f26f5cea90f9
Luis Gabriel Janco <janco7249@gmail.com>
2023-12-28 14:54:25 -0400

    feat: add page not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
  */
 export default function NotFound() {
   return (
-    <div className="py-20 px-6 max-w-screen-xl mx-auto dark:text-gray-300 text-gray-500 grid place-content-center place-items-center w-full h-screen">
-      <h2 className="text-xl md:text-3xl font-extrabold">Not Found</h2>
-      <p>Could not find requested resource</p>
+    <div className="py-20 px-6 max-w-screen-xl mx-auto dark:text-gray-300 text-gray-500 grid place-content-center place-items-center w-full h-screen z-0 relative">
+      <h2 className="text-xl md:text-3xl font-extrabold">No encontrado</h2>
+      <p>No se pudo encontrar la página</p>
       <Link className="dark:hover:text-white hover:text-black" href="/">
-        Return Home
+        Volver al inicio
       </Link>
     </div>
   );
